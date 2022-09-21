5198700c5606a9592dea8ca501d26c3d1701e331
Andrew <afellman84@gmail.com>
2022-09-20 22:10:54 -0400

    commit tree 731bfa49c37b957d1e6d01b2d4a6b393bb9b7ece

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+// const someValue1 = 199
+// const someValue2 = 1500
+// const someValue3 = 2000
+// const someValue4 = 2500
+//  function scuberGreetingForFeet(someValue1) {
+//   // Write your code here!
+//   let result
+//   if (someValue1 <= 400) {
+//   result = "This one is on me!"
+// }
+//   return result
+  
+  function scuberGreetingForFeet(someValue2) {
+    let result
+    if (someValue2 <= 400) {
+    result = "This one is on me!"}
+     else if (someValue2 > 400 && someValue2 < 2000) {
+      result = "That will be twenty bucks."
+    } else if (someValue2 > 2000 && someValue2 < 2500) {
+      result = "I will gladly take your thirty bucks." 
+    } else {
+      result = "No can do."
+    }
+    return result
+  }
 
-function ternaryCheckCity(){
+  
+  
+
+function ternaryCheckCity(city){
   // Write your code here!
+  
+  const cityMessage = city === "NYC" ? "Ok, sounds good." : "No go."
+  return cityMessage
 }
 
 function switchOnCharmFromTip(){
